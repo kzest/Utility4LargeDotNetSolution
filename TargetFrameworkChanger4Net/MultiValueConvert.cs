7601753b30621ae303ce19bc995feb29a83a047e
Sarthak Tak <sarthakns@gmail.com>
2018-06-26 15:45:13 +0530

    Add files via upload

---
+++ TargetFrameworkChanger4Net/MultiValueConvert.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TargetFrameworkChanger4Net.Model;
+
+namespace TargetFrameworkChanger4Net
+{
+    public class MultiValueConvert : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            dynamic expando = new ExpandoObject();
+            int iCount = values.Length;
+            for (int i = 0; i < iCount; i++)
+            {
+                object val = values[i];
+
+                if(parameter.ToString() == "SelectedProject")
+                {
+                    expando.ProjectModelType = (ProjectModel.ProjectModelType)values[0];
+                    expando.ProjectGuid = values[1] as string;
+                    expando.IsSelected = (bool)values[2];
+                    expando.RootWindow = values[3];
+                    break;
+                }
+                else if(parameter.ToString() == "SelectAll")
+                {
+                    expando.IsChecked = (bool)values[0];
+                    expando.ListView = (System.Windows.Controls.ListView)values[1];
+                    expando.RootWindow = values[2];
+                    break;
+                }
+                else if(parameter.ToString() == "ChangeTargetFrameworkVersions")
+                {
+                    expando.CurrentTargetFrameworkVersion = values[0] as string;
+                    expando.ListView = (System.Windows.Controls.ListView)values[1];
+                    expando.RootWindow = values[2];
+                    break;
+                }
+                else
+                {
+                    (expando as IDictionary<string, object>).Add(val.GetType().Name, val);
+                }                
+            }
+
+            return expando;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new object[0];
+        }
+    }
+}
---
+++ TargetFrameworkChanger4Net/RelayCmd.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TargetFrameworkChanger4Net
+{
+    public class RelayCmd : ICommand
+    {
+        public Func<object, bool> _canExecute { get; set; }
+        public Action<object> _execute { get; set; }
+
+        public RelayCmd(Func<object, bool> canExecute, Action<object> execute)
+        {
+            this._canExecute = canExecute;
+            this._execute = execute;
+        }
+
+        #region ICommand
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (this._canExecute != null)
+                return this._canExecute(parameter);
+            else
+                return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (this._execute != null)
+                this._execute(parameter);
+        }
+        #endregion
+    }
+}
